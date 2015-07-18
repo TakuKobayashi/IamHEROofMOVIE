6dea99bc36e2f8487e4c3652a35c8d7953f25664
TakuKobayashi <keep_slimbody@yahoo.co.jp>
2015-07-18 19:10:16 +0900

    mqttするためのライブラリとか検証を追加

--- app/src/main/java/com/movie/of/hero/am/i/iamheroofmovie/MainActivity.java
+++ app/src/main/java/com/movie/of/hero/am/i/iamheroofmovie/MainActivity.java
@@ -2,16 +2,63 @@ package com.movie.of.hero.am.i.iamheroofmovie;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import org.eclipse.paho.android.service.MqttAndroidClient;
+import org.eclipse.paho.client.mqttv3.IMqttActionListener;
+import org.eclipse.paho.client.mqttv3.IMqttToken;
+import org.eclipse.paho.client.mqttv3.MqttConnectOptions;
+import org.eclipse.paho.client.mqttv3.MqttException;
+import org.eclipse.paho.client.mqttv3.MqttPersistenceException;
+
+import java.util.UUID;
+
 
 public class MainActivity extends Activity {
+
+    //Youtube のビデオID
+    private static String videoId = "9bZkp7q19f0";
+    private MqttAndroidClient mqttAndroidClient;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        String clientId = UUID.randomUUID().toString();
+        mqttAndroidClient = new MqttAndroidClient(this, "tcp://100.123.45.194:61623", "webconsole-88028"); // (1)
+        MqttConnectOptions options = new MqttConnectOptions();
+        options.setUserName("admin");
+        options.setPassword("password".toCharArray());
+        //connecしてpublisする処理だが,connecができていないのでコメントアウト
+        /*
+        try {
+            mqttAndroidClient.connect(options, null, new IMqttActionListener() {
+
+                @Override
+                public void onSuccess(IMqttToken mqttToken) {
+                    Log.d("TakuTaku", "SUCCESS:" + mqttToken.getMessageId());
+                }
+
+                @Override
+                public void onFailure(IMqttToken arg0, Throwable arg1) {
+                    Log.d("TakuTaku", "FAIL:" + arg1.getMessage());
+                }
+            });
+        } catch (MqttException e) {
+            Log.d("TakuTaku", "error:" + e.getMessage());
+            e.printStackTrace();
+        }
+        Log.d("TakuTaku", "client:" + mqttAndroidClient.isConnected());
+        try {
+            mqttAndroidClient.publish("3dbcs.biz/UTokyo/iREF/6F/Hilobby/Light/LED/LoE/L42/brightness/W", "value=0&datetime=2015-07-18T17:14:00+09:00".getBytes(), 0, false); // (3)
+        } catch (MqttPersistenceException e) {
+            e.printStackTrace();
+        } catch (MqttException e) {
+            e.printStackTrace();
+        }
+        */
     }
 
     @Override
@@ -34,5 +81,10 @@ public class MainActivity extends Activity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
     }
 }
